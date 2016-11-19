e5e031690ebdfff676228e8bf7886bf3f85a7636
Carlo Peroni <carloperoni.italy@gmail.com>
2016-11-19 17:00:24 +0100

    tank's barrel bug fixed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', 
-                                       { preload: preload, create: create, update: update, render: render })
+									   { preload: preload, create: create, update: update, render: render })
 
 var player;
 var barrel;
@@ -13,51 +13,42 @@ function create() {
 	player = game.add.sprite(200, 200, 'tank');
 	player.scale.setTo(0.5, 0.5);
 	player.anchor.setTo(0.5, 0.5);
-    barrel = new Phaser.Sprite(this.game, 0, 0, "barrel");  
-	player.addChild(barrel);
+	barrel = game.add.sprite(200,200,"barrel")
 	barrel.anchor.setTo(0,0.5)
+	barrel.scale.setTo(0.5,0.5)
 
 }
 
 function update() {
 
 
-    if (game.input.keyboard.isDown(Phaser.KeyCode.W))
-    {
+	if (game.input.keyboard.isDown(Phaser.KeyCode.W))
+	{
 		speed = 3
-        var velocityX = Math.cos(player.angle * Math.PI / 180) * speed;
-        var velocityY = Math.sin(player.angle * Math.PI / 180) * speed;
-        player.x += velocityX;
-        player.y += velocityY;	
+		var velocityX = Math.cos(player.angle * Math.PI / 180) * speed;
+		var velocityY = Math.sin(player.angle * Math.PI / 180) * speed;
+		player.x += velocityX;
+		player.y += velocityY;	
+		barrel.x = player.x
+		barrel.y = player.y
 	}
 		
 	if (game.input.keyboard.isDown(Phaser.KeyCode.D)) 
 	{
-		player.angle += 1;
+		player.angle += 2;
 	}
 		
 	if (game.input.keyboard.isDown(Phaser.KeyCode.A))
 	{
-		player.angle -= 1;
+		player.angle -= 2;
 	}
 	
-	if (game.input.keyboard.isDown(Phaser.KeyCode.S))
-    {
-        speed = -2;
-		var velocityX = Math.cos(player.angle * Math.PI / 180) * speed;
-        var velocityY = Math.sin(player.angle * Math.PI / 180) * speed;
-        player.x += velocityX;
-        player.y += velocityY;
-	}
-	
-	
-	 //barrel.rotation = game.physics.arcade.angleToPointer(barrel);
-	 barrel.angle += 1
+	 barrel.rotation = game.physics.arcade.angleToPointer(barrel);   
 
 }
 
 function render() {
-    game.debug.spriteInfo(barrel, 20, 32);
+	game.debug.spriteInfo(player, 20, 32);
 	
 
 }
